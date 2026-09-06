b7eebb532c39446af5b0a3b2230beca57fe2d32b
conradipui-glitch <conradipui@gmail.com>
2026-09-07 00:34:23 +0600

    feat(player): export frozen playtest bootstrap

---
+++ packages/player/src/index.ts
@@ -0,0 +1,8 @@
+export {
+  bootstrapFrozenPlaytest,
+  type BootstrapFrozenPlaytestResult,
+  type FrozenPlaytestBootstrapSource,
+  type FrozenPlaytestSnapshotSource,
+  type PlayerPaintActionDefinition,
+  type PlayerRuntimeTemplate
+} from "./bootstrap.js";
