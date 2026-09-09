238a40e4cce4d38068edfa1a2cd38213e66447de
conradipui-glitch <conradipui@gmail.com>
2026-09-09 11:17:38 +0600

    B13.a2: export bounded agent job

--- apps/builder-runner/src/index.ts
+++ apps/builder-runner/src/index.ts
@@ -4,6 +4,9 @@
 export { applyBoundedPatch, BoundedPatchError } from "./bounded-patch-job.js";
 export type { MutableBuilderWorkspace, PatchEvidence, PatchOperation } from "./bounded-patch-job.js";
 
+export { runBoundedAgentJob } from "./bounded-agent-job.js";
+export type { BoundedAgentJobResult, BuilderVerificationRunner } from "./bounded-agent-job.js";
+
 export { createBuilderWorkspacePolicy, authorizeBuilderPath } from "./workspace-policy.js";
 export type { BuilderWorkspacePolicy } from "./workspace-policy.js";
 
